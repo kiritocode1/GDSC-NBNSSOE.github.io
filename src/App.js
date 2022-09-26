5a0bb46e5dfb07ed1fc899986d42b354f05e81f5
kiritocode1 <kathawalearyan9@gmail.com>
2022-09-26 13:48:10 +0530

    Redirect done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className=" lol">
+      Click 👉 <a href="https://gdsc-nbnssoe.github.io/gdsc-frontend">here</a> 
     </div>
   );
 }
